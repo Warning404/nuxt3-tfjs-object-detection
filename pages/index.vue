2b9291ab96d23fe8bf1bf39b8256947230ab8ab8
Warning404 <mai.rok101@gmail.com>
2024-05-23 19:17:47 +0700

    modified:   pages/index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,111 +1,112 @@
 <template>
   <div class="object-detector-container">
+
     <button @click="openFilePicker" :disabled="isLoading">{{ isLoading ? 'Recognizing...' : 'Select Image' }}</button>
-
+    {{ predictions }}
     <div class="detector-container">
-
-      <img v-if="imgData" :src="imgData" ref="imageRef" alt="Target Image" />
+      <img v-if="imgData" :src="imgData" ref="imageRef" alt="Target Image" width="100%" />
       <div v-if="!isEmptyPredictions" v-for="(prediction, idx) in predictions" :key="idx" class="target-box"
         :style="{ left: prediction.bbox[0] + 'px', top: prediction.bbox[1] + 'px', width: prediction.bbox[2] + 'px', height: prediction.bbox[3] + 'px' }">
         {{ prediction.class }} {{ (prediction.score * 100).toFixed(1) }}%
       </div>
     </div>
     <input type="file" @change="onSelectImage" ref="fileInputRef" style="display: none;" />
-
   </div>
 </template>
 
-<script>
+<script setup>
+
 import * as cocoSsd from "@tensorflow-models/coco-ssd";
 import "@tensorflow/tfjs-backend-cpu";
 
-export default {
-  data() {
-    return {
-      fileInputRef: null,
-      imageRef: null,
-      imgData: null,
-      predictions: [],
-      isLoading: false
-    };
-  },
-  computed: {
-    isEmptyPredictions() {
-      return !this.predictions || this.predictions.length === 0;
-    }
-  },
-  methods: {
-    async openFilePicker() {
-      this.$refs.fileInputRef.click();
-    },
-    async normalizePredictions(predictions, imgSize) {
-      if (!predictions || !imgSize || !this.$refs.imageRef) return predictions || [];
-      return predictions.map((prediction) => {
-        const { bbox } = prediction;
-        const oldX = bbox[0];
-        const oldY = bbox[1];
-        const oldWidth = bbox[2];
-        const oldHeight = bbox[3];
 
-        const imgWidth = this.$refs.imageRef.width;
-        const imgHeight = this.$refs.imageRef.height;
 
-        const x = (oldX * imgWidth) / imgSize.width;
-        const y = (oldY * imgHeight) / imgSize.height;
-        const width = (oldWidth * imgWidth) / imgSize.width;
-        const height = (oldHeight * imgHeight) / imgSize.height;
+const fileInputRef = ref(null);
+const imageRef = ref(null);
+const imgData = ref(null);
+const predictions = ref([]);
+const isLoading = ref(false);
 
-        return { ...prediction, bbox: [x, y, width, height] };
-      });
-    },
-    async detectObjectsOnImage(imageElement, imgSize) {
-      const model = await cocoSsd.load({});
-      const predictions = await model.detect(imageElement, 6);
-      const normalizedPredictions = await this.normalizePredictions(predictions, imgSize);
-      console.log(predictions)
-      
-      this.predictions = normalizedPredictions;
-    },
-    readImage(file) {
-      return new Promise((resolve, reject) => {
-        const fileReader = new FileReader();
-        fileReader.onload = () => resolve(fileReader.result);
-        fileReader.onerror = () => reject(fileReader.error);
-        fileReader.readAsDataURL(file);
-      });
-    },
-    async onSelectImage(e) {
-      this.predictions = [];
-      this.isLoading = true;
+const isEmptyPredictions = computed(() => {
+  return !predictions.value || predictions.value.length === 0;
+});
 
-      const file = e.target.files[0];
-      const imgData = await this.readImage(file);
-      this.imgData = imgData;
-
-      const imageElement = document.createElement("img");
-      imageElement.src = imgData;
-
-      imageElement.onload = async () => {
-        const imgSize = {
-          width: imageElement.width,
-          height: imageElement.height,
-        };
-        await this.detectObjectsOnImage(imageElement, imgSize);
-        this.isLoading = false;
-      };
-    }
-  }
+const openFilePicker = () => {
+  fileInputRef.value.click();
 };
+
+const normalizePredictions = async (predictions, imgSize) => {
+  if (!predictions || !imgSize || !imageRef.value) return predictions || [];
+  return predictions.map((prediction) => {
+    const { bbox } = prediction;
+    const oldX = bbox[0];
+    const oldY = bbox[1];
+    const oldWidth = bbox[2];
+    const oldHeight = bbox[3];
+
+    const imgWidth = imageRef.value.width;
+    const imgHeight = imageRef.value.height;
+
+    const x = (oldX * imgWidth) / imgSize.width;
+    const y = (oldY * imgHeight) / imgSize.height;
+    const width = (oldWidth * imgWidth) / imgSize.width;
+    const height = (oldHeight * imgHeight) / imgSize.height;
+
+    return { ...prediction, bbox: [x, y, width, height] };
+  });
+};
+
+const detectObjectsOnImage = async (imageElement, imgSize) => {
+  const model = await cocoSsd.load({});
+  const results = await model.detect(imageElement, 6);
+  const normalizedResults = await normalizePredictions(results, imgSize);
+  predictions.value = normalizedResults;
+};
+
+const readImage = (file) => {
+  return new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.onload = () => resolve(fileReader.result);
+    fileReader.onerror = () => reject(fileReader.error);
+    fileReader.readAsDataURL(file);
+  });
+};
+
+const onSelectImage = async (e) => {
+  predictions.value = [];
+  isLoading.value = true;
+
+  const file = e.target.files[0];
+  const imageData = await readImage(file);
+  imgData.value = imageData;
+
+  const imageElement = document.createElement("img");
+  imageElement.src = imageData;
+
+  imageElement.onload = async () => {
+    const imgSize = {
+      width: imageElement.width,
+      height: imageElement.height,
+    };
+    await detectObjectsOnImage(imageElement, imgSize);
+    isLoading.value = false;
+  };
+};
+
+
+
 </script>
 
 <style scoped>
 .object-detector-container {
+
   display: flex;
   flex-direction: column;
   align-items: center;
 }
 
 .detector-container {
+  margin-top: 100px;
   min-width: 200px;
   height: 700px;
   border: 3px solid #fff;
